12402c8c1c283b0449b61a9765dc96660b21d195
AleOlea <espiritusdelfuego@gmail.com>
2021-06-17 09:15:50 -0400

    Docs: Implementing Firebase

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,13 +1,41 @@
+import { useCollectionOnce } from 'react-firebase-hooks/firestore';
 import React from "react";
 // import{useState} from "react"
 // import './App.css';
 import Check from "./Check"
+import db from "../config"
 
 function Table() {
+  const query = db.collection('products')
+  const [values, loading, error] = useCollectionOnce(query);
+  const option = {
+    idField: "id",
+   
+  };
+
+    console.log(values)
  
 
   return (
     <div className="App">
+        <div>
+      <p>
+        {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Collection: Loading...</span>}
+        {values && (
+          <span>
+            Collection:{' '}
+            //if values .doc exist...
+            {values.docs.map((doc) => (
+              <React.Fragment key={doc.id}>
+                {JSON.stringify(doc.data())},{' '}
+              </React.Fragment>
+            ))}
+          </span>
+        )}
+      </p>
+    </div>
+
       <header className="cloud-cafe"> Cloud Cafe </header>
       <div className = "cafes">
       <h2>Cafes</h2>
